4b2221564973fa95f94a3e84f1f97e1a8ba3c8d7
thejas-1999 <thejask074@gmail.com>
2024-03-07 11:21:53 +0530

    create  the functon for accessing the files

--- server.js
+++ server.js
@@ -8,13 +8,28 @@ const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
   console.log("request date:" + new Date());
-  res.send("Welcome middleware app");
+
   next();
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
+app.use((req, res, next) => {
+  var filepath = path.join(__dirname, "static", req.url);
+  fs.stat(filepath, (err, fileInfo) => {
+    if (err) {
+      next();
+      return;
+    }
+    if (fileInfo.isFile()) {
+      res.sendFile(filepath);
+    } else {
+      next();
+    }
+  });
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.send("File is not found");
 });
 
 app.listen(PORT, () => {
